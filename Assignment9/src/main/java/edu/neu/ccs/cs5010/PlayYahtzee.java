0ad3057d5231b31d0df9588f49ffd435e5007cd5
peishanwang <wang.peish@husky.neu.edu>
2017-12-01 23:14:37 -0800

    fix bug

--- Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
+++ Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
@@ -1,8 +1,6 @@
 package edu.neu.ccs.cs5010;
 
 
-import edu.neu.cs5010.yahtzee.messages.ServerMessageParser;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -42,7 +40,7 @@ public class PlayYahtzee {
           System.out.println("Server: " + fromServer);
           if (fromServer.contains("GAME_OVER")) {
             kkSocket.close();
-            System.out.println("fromServer is null, so closing the socket... ");
+            System.out.println("Game is over, bye!");
             return;
           }
           String response = this.getResponse(fromServer);
@@ -64,28 +62,22 @@ public class PlayYahtzee {
 
   private String getResponse(String fromServer) {
     String[] out = fromServer.split(":");
-    ServerMessageParser.Frame frame = ServerMessageParser.Frame.fromString(out[0].trim());
+    FrameParser.Frame frame = FrameParser.getFrame(out[0]);
     switch(frame) {
       case CHOOSE_SCORE:
         return this.getKeepScore(fromServer);
       case SCORE_CHOICE_INVALID:
         return this.getKeepScore(fromServer);
       case CHOOSE_DICE:
-        String userIn = this.getKeepDiceFrame("", fromServer);
-        return userIn;
-      case INFO:
-        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.ACK, "ok.");
-      case START_TURN:
-        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.PRINT_GAME_STATE, "pls");
+        return this.getKeepDiceFrame(out[1]);
       default:
-        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.ACK, "ok.");
+        return "INFO: wow";
     }
   }
 
   private String getKeepScore(String fromServer) {
     try {
-      System.out.println("Enter  the Score you want to take:  " + fromServer);
-
+      System.out.println("Enter the Score you want to take:  " + fromServer);
       String userIn;
       for(userIn = this.stdIn.readLine(); userIn.length() <= 1; userIn = this.stdIn.readLine()) {
         System.out.println("Please enter something  ");
@@ -98,17 +90,17 @@ public class PlayYahtzee {
     }
   }
 
-  private String getKeepDiceFrame(String fromServer, String userIn) {
+  private String getKeepDiceFrame(String diceValue) {
     try {
       System.out.println("Enter numbers for the ones you want to keep, 1-5. ");
-      userIn = this.stdIn.readLine();
+      String userIn = this.stdIn.readLine();
       String[] vals = userIn.trim().split(" ");
       int[] out = new int[]{0, 0, 0, 0, 0};
-      StringBuffer buffer = new StringBuffer("KEEP_DICE: 4 4 2 2 4");
+      StringBuffer buffer = new StringBuffer("KEEP_DICE: " + diceValue);
       int i;
       if (userIn.trim().length() > 0) {
         for(i = 0; i < vals.length; ++i) {
-          int val = Integer.valueOf(vals[i]).intValue();
+          int val = Integer.valueOf(vals[i]);
           if (val >= 1 && val <= 5) {
             out[val - 1] = 1;
           }
