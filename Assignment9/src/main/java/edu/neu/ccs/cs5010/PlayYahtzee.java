9d5239d516809b0e21215508ff0fc6a9aba29234
peishanwang <wang.peish@husky.neu.edu>
2017-12-01 21:56:34 -0800

    fix null exception

--- Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
+++ Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
@@ -1,6 +1,7 @@
 package edu.neu.ccs.cs5010;
 
-import edu.neu.cs5010.yahtzee.client.GameClient;
+
+import edu.neu.cs5010.yahtzee.messages.ServerMessageParser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,7 +22,7 @@ public class PlayYahtzee {
 
     String hostName = args[0];
     int portNumber = Integer.parseInt(args[1]);
-    new GameClient(hostName, portNumber);
+    new PlayYahtzee(hostName, portNumber);
   }
 
   public PlayYahtzee(String hostName, int portNumber) {
@@ -39,15 +40,14 @@ public class PlayYahtzee {
         String fromServer;
         while ((fromServer = in.readLine()) != null) {
           System.out.println("Server: " + fromServer);
-          String response = this.getResponse(fromServer);
-          if (response.isEmpty()) {
-            break;
+          if (fromServer.contains("GAME_OVER")) {
+            kkSocket.close();
+            System.out.println("fromServer is null, so closing the socket... ");
+            return;
           }
+          String response = this.getResponse(fromServer);
           out.println(response);
         }
-
-        kkSocket.close();
-        System.out.println("fromServer is null, so closing the socket... ");
         Thread.sleep(100L);
       }
     } catch (UnknownHostException var68) {
@@ -64,63 +64,58 @@ public class PlayYahtzee {
 
   private String getResponse(String fromServer) {
     String[] out = fromServer.split(":");
-    String frame = (out[0].trim());
-    if (frame == null || frame.isEmpty()) {
-      return "";
-    }
-    switch (frame) {
-      case "CHOOSE_SCORE" :
+    ServerMessageParser.Frame frame = ServerMessageParser.Frame.fromString(out[0].trim());
+    switch(frame) {
+      case CHOOSE_SCORE:
         return this.getKeepScore(fromServer);
-      case "SCORE_CHOICE_INVALID" :
+      case SCORE_CHOICE_INVALID:
         return this.getKeepScore(fromServer);
-      case "CHOOSE_DICE" :
-        return this.getKeepDiceFrame(fromServer);
-      case "INFO" :
-        return "INFO: ok.";
-      case "START_TURN" :
-        return "START_TURN: pls";
-      case "START_ROUND" :
-        return "START_ROUND: pls";
-      case "GAME_OVER" :
-        return "GAME_OVER: bye bye!";
+      case CHOOSE_DICE:
+        String userIn = this.getKeepDiceFrame("", fromServer);
+        return userIn;
+      case INFO:
+        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.ACK, "ok.");
+      case START_TURN:
+        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.PRINT_GAME_STATE, "pls");
       default:
-        return "INFO: oh!";
+        return ServerMessageParser.getSingleton().createMessage(ServerMessageParser.Frame.ACK, "ok.");
     }
   }
 
   private String getKeepScore(String fromServer) {
     try {
-      System.out.println("Enter the Score you want to take:  " + fromServer);
+      System.out.println("Enter  the Score you want to take:  " + fromServer);
+
       String userIn;
-      for (userIn = this.stdIn.readLine(); userIn.length() <= 1; userIn = this.stdIn.readLine()) {
-        System.out.println("Please enter something");
+      for(userIn = this.stdIn.readLine(); userIn.length() <= 1; userIn = this.stdIn.readLine()) {
+        System.out.println("Please enter something  ");
       }
 
-      return "CHOOSE_SCORE: " + userIn;
+      return "SCORE_CHOICE: " + userIn;
     } catch (IOException var3) {
       var3.printStackTrace();
       return "2: 1 1 1 1 1";
     }
   }
 
-  private String getKeepDiceFrame(String fromServer) {
+  private String getKeepDiceFrame(String fromServer, String userIn) {
     try {
       System.out.println("Enter numbers for the ones you want to keep, 1-5. ");
-      String userIn = this.stdIn.readLine();
+      userIn = this.stdIn.readLine();
       String[] vals = userIn.trim().split(" ");
       int[] out = new int[]{0, 0, 0, 0, 0};
-      StringBuffer buffer = new StringBuffer(fromServer);
+      StringBuffer buffer = new StringBuffer("KEEP_DICE: 4 4 2 2 4");
       int i;
       if (userIn.trim().length() > 0) {
-        for (i = 0; i < vals.length; ++i) {
-          int val = Integer.valueOf(vals[i]);
+        for(i = 0; i < vals.length; ++i) {
+          int val = Integer.valueOf(vals[i]).intValue();
           if (val >= 1 && val <= 5) {
             out[val - 1] = 1;
           }
         }
       }
 
-      for (i = 0; i < out.length; ++i) {
+      for(i = 0; i < out.length; ++i) {
         if (out[i] == 1) {
           buffer.append(" 1");
         } else {
