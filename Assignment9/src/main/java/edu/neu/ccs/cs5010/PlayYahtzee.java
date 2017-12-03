c979e7e8d7339cfed0e2c089c43578b344f0eb9d
Manika <manikasharma2211@gmail.com>
2017-12-03 14:53:23 -0800

    numberformatexception caught and processed properly

--- Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
+++ Assignment9/src/main/java/edu/neu/ccs/cs5010/PlayYahtzee.java
@@ -125,12 +125,19 @@ public class PlayYahtzee {
         String[] vals = userIn.trim().split(" ");
         if (userIn.trim().length() > 0) {
           for(int i = 0; i < vals.length; ++i) {
-            int val = Integer.valueOf(vals[i]);
-            if (val >= 1 && val <= 5) {
-              out[val - 1] = 1;
-            } else {
-              System.out.println("INFO : Enter a number between 1 and 5.");
+            try {
+              int val = Integer.valueOf(vals[i]);
+              if (val >= 1 && val <= 5) {
+                out[val - 1] = 1;
+              } else {
+                System.out.println("INFO : Enter a number between 1 and 5.");
+                continue OUTER;
+              }
+            }
+            catch (NumberFormatException ex){
+              System.out.println("Wrong entry. String not allowed. Enter a number between 1 and 5");
               continue OUTER;
+
             }
           }
         }
